a0b21895f34bf320ab50ae954633a80ca1bbee8a
Moaz Osama <moazosama204@gmail.com>
2026-07-02 07:55:58 -0700

    add sizes for products for dtos and entities

---
+++ Drape/Home/Data/DTOs/OptionDTO.swift
@@ -0,0 +1,14 @@
+//
+//  OptionDTO.swift
+//  Drape
+//
+//  Created by Moaz on 02/07/2026.
+//
+
+import Foundation
+
+
+struct OptionDTO: Codable {
+    let name: String
+    let values: [String]
+}
