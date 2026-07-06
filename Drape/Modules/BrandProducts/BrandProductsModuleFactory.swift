994c3c03d2d8b9cdc112313de6c49cf1e783a176
Youssef Abd El-Fatah <99736061+YoussefAbdel-Fatah@users.noreply.github.com>
2026-07-05 18:56:39 -0700

    Update BrandProductsModuleFactory.swift
    change brand from Brand type to String type in factory

--- Drape/Modules/BrandProducts/BrandProductsModuleFactory.swift
+++ Drape/Modules/BrandProducts/BrandProductsModuleFactory.swift
@@ -10,13 +10,13 @@ import SwiftData
 
 enum BrandProductsModuleFactory {
     @MainActor
-    static func makeView(brand: Brand, modelContext: ModelContext) -> BrandProductsView {
+    static func makeView(brand: String, modelContext: ModelContext) -> BrandProductsView {
         let savedProductsRepository = SavedProductsRepositoryImpl(context: modelContext)
         let toggleUseCase = ToggleSaveProductUseCase(repository: savedProductsRepository)
         let getSavedUseCase = GetSavedProductsUseCase(repository: savedProductsRepository)   // NEW
 
         let viewModel = BrandProductsViewModel(
-            brandName: brand.name,
+            brandName: brand,
             toggleSaveProductUseCase: toggleUseCase,
             getSavedProductsUseCase: getSavedUseCase
         )
